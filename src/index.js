84b3f8c7336a3ad22b3d8b09a8f5312701de39f8
fischerdavis <fischerdavis94@gmail.com>
2020-11-22 22:12:27 -0600

    Refactor of three components into one

--- src/index.js
+++ src/index.js
@@ -8,11 +8,6 @@ function Booklist() {
   return (
     <section className="bookList">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
@@ -20,16 +15,17 @@ function Booklist() {
 function Book () {
   return (
   <article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt="" />
+    <h1>I Love You to the Moon and Back</h1>
+    <h4>Amelia Hepworth</h4>
   </article>
   );
 }
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt="" />
+// const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt="" />
+// In style, {{}} the outer {} just means that we are back in javascript world,
+// the inner {} means that it is an object.
+// Also side note -> inline css takes prescedence over css imported from a file.
+// const Author = () => <h4 style={{ color: '#617d98', fontSize: '0.75rem',margin: '0.25rem' }}>Amelia Hepworth</h4>
 
-const Title = () => <h1>I Love You to the Moon and Back</h1>
-
-const Author = () => <h4>Amelia Hepworth</h4>
 ReactDom.render(<Booklist />, document.getElementById("root"));
