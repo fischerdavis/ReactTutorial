0add2ea94a6200f654bc072181ffbfd8975065b5
fischerdavis <fischerdavis94@gmail.com>
2020-11-29 17:16:25 -0600

    Progress

--- src/index.js
+++ src/index.js
@@ -4,18 +4,29 @@ import ReactDom from "react-dom";
 // CSS
 import './index.css';
 
-firstBook = {
+const firstBook = {
   img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
   title: 'I Love You to the Moon and Back',
   author: 'Amelia Hepworth'
-}
+};
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+  title: 'Our Class is a Family',
+  author: 'Shannon Olsen'
+};
 
 
 function Booklist() {
   return (
     <section className="bookList">
-      <Book firstBook/>
-      <Book title='Title' number={23}/>
+      <Book img={firstBook.img}
+            title={firstBook.title}
+            author={firstBook.author}
+      />
+      <Book img={secondBook.img}
+            title={secondBook.title}
+            author={secondBook.author}
+      />
     </section>
   );
 }
